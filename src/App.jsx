ec43258e11e35a1f43bb5c12b2bd5dca282df991
shwg8986 <67375194+shwg8986@users.noreply.github.com>
2022-05-08 19:29:37 +0900

    React practice

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,14 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>
+          登録できるTODOは5個までです。消化してください。
+        </p>
+      )}
 
       <IncompleteTodos
         todos={incompleteTodos}
